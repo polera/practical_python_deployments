f0241ce9e09f5c6df27b87a79508b43cba094729
James Polera <james@uncryptic.com>
2012-02-12 22:13:07 -0500

    Added nameparts and some app.py, requirements.txt changes.

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from nameparts import Name
+from json import dumps
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "This is the index page."
+    n = Name("James Polera")
+    return dumps(n.as_dict)
 
 if __name__ == "__main__":
     app.run()
