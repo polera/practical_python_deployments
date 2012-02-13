2799b5594b98af6b645d1e0e52e7662925e26599
James Polera <james@uncryptic.com>
2012-02-12 23:56:13 -0500

    Updated for heroku

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 from nameparts import Name
-from json import dumps
+import os
 
 app = Flask(__name__)
 
@@ -18,4 +18,5 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000)) 
+    app.run(host='0.0.0.0', port=port)
